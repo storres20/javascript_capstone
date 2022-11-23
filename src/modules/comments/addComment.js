de8faa1e82cdbe2ead1cb88b04391f0980a9bf52
storres20 <sanchotorres777@gmail.com>
2022-11-23 16:53:54 -0500

    Update addComment module

--- src/modules/comments/addComment.js
+++ src/modules/comments/addComment.js
@@ -1,4 +1,7 @@
-const addComment = async (newComment, url) => {
+import showComment from './showComment.js';
+
+// POST comment to API
+const addComment = async (newComment, url, id) => {
   await fetch(url, {
     method: 'POST',
     body: JSON.stringify(newComment),
@@ -6,6 +9,9 @@ const addComment = async (newComment, url) => {
       'Content-type': 'application/json; charset=UTF-8',
     },
   });
+
+  // Show Comments after adding a new one on modal screen
+  showComment(id, url);
 };
 
 export default addComment;
