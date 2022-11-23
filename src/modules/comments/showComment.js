035585b865fb1446ac246571b8c4ae639e2651ae
storres20 <sanchotorres777@gmail.com>
2022-11-23 16:54:45 -0500

    Update showComment module

--- src/modules/comments/showComment.js
+++ src/modules/comments/showComment.js
@@ -1,12 +1,23 @@
-const showComment = async (id, url) => {
-  const response = await fetch(`${url}?item_id=${id}`);
-  const data = await response.json();
+import getComment from './getComment.js';
 
-  if (data.error) {
-    return 0;
-  }
+// Render comments on modal screen
+const showComment = (id, url) => {
+  const comments = document.getElementById(`comment${id}`);
 
-  return data;
+  getComment(id, url)
+    .then((data) => {
+      // If this "item_id" have comments
+      comments.innerHTML = `<h4 class="text-center">Comments (${data.length})</h4>`;
+      data.forEach((item) => {
+        comments.innerHTML += `
+        <p>${item.creation_date} ${item.username}: ${item.comment}</p>
+      `;
+      });
+    })
+    .catch((error) => {
+      // If this "item_id" doesn't exist or don't have comments
+      comments.innerHTML = `<h4 class="text-center">Comments (${error.message})</h4>`;
+    });
 };
 
 export default showComment;
